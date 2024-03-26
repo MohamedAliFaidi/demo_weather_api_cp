bd592b348c7b45759114551e730966c61db182d0
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-03-26 19:45:23 +0100

    clean up

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 require("dotenv").config();
 app.use(express.static("public"));
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
 
 app.get("/", (req, res) => {
   res.sendFile("public/index.html")
